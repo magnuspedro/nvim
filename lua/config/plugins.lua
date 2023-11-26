a1bb3d34c0dc259c39626d4efac361d2c97a4de5
magnus <pedmagnus@gmail.com>
2023-11-26 20:04:40 -0300

    Adding autopairs plugin

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -10,7 +10,8 @@ return require('packer').startup(function()
     use 'hrsh7th/nvim-cmp'
     use 'saadparwaiz1/cmp_luasnip'
     use 'L3MON4D3/LuaSnip'
-    use { 'nvim-tree/nvim-tree.lua', requires = { 'nvim-tree/nvim-web-devicons' }, file_ignore_patterns = { "node_modules", "target" } }
+    use { 'nvim-tree/nvim-tree.lua', requires = { 'nvim-tree/nvim-web-devicons' }, file_ignore_patterns = {
+        "node_modules", "target" } }
     use 'udalov/kotlin-vim'
     use { 'nvim-telescope/telescope.nvim', tag = '0.1.0', requires = { { 'nvim-lua/plenary.nvim' } }
     }
@@ -18,4 +19,8 @@ return require('packer').startup(function()
     use 'terrortylor/nvim-comment'
     use 'mfussenegger/nvim-dap'
     use 'alexghergh/nvim-tmux-navigation'
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
 end)
