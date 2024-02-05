9c68084323391bccf0fbfdadb9d1383f3a3d5401
magnus <pedmagnus@gmail.com>
2024-02-05 00:51:00 -0300

    changing gr to go to definition

--- lua/config/lsp/config/settings.lua
+++ lua/config/lsp/config/settings.lua
@@ -16,8 +16,9 @@ local on_attach = function(client, bufnr)
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
---    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.references, bufopts) -- Change to references
+    vim.keymap.set('n', 'gd', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.definition, bufopts)
+
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
@@ -29,7 +30,6 @@ local on_attach = function(client, bufnr)
     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
     vim.keymap.set('n', '<A-CR>', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
     vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
