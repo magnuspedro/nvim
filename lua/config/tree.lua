fa1cce3a2a3374567d05c48d89e10e1aec072ba5
magnus <pedmagnus@gmail.com>
2024-02-05 00:04:32 -0300

    adding find file on tree

--- lua/config/tree.lua
+++ lua/config/tree.lua
@@ -7,17 +7,18 @@ vim.opt.termguicolors = true
 
 -- toggle tree
 vim.keymap.set('n', '<c-Bslash>', ':NvimTreeToggle<CR>', { silent = true })
+vim.keymap.set('n', '<leader>t', ':NvimTreeFindFile<CR>', { silent = true })
 
 -- OR setup with some options
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = false,
-  },
+    sort_by = "case_sensitive",
+    view = {
+        width = 30,
+    },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = false,
+    },
 })
