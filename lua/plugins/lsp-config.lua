f295bb49218772e10b75006e7a9ef5489abaf3a1
magnus <pedmagnus@gmail.com>
2024-05-21 16:57:01 -0300

    adding more stuff

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,76 +1,80 @@
 return {
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = {
-			"williamboman/mason.nvim",
-			opts = {
-				registries = {
-					"github:nvim-java/mason-registry",
-					"github:mason-org/mason-registry",
-				},
-			},
-		},
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"jdtls",
-					"tsserver",
-					"pyright",
-					"hls",
-					"terraformls",
-					"kotlin_language_server",
-					"gopls",
-				},
-			})
-		end,
-	},
-	{ "mfussenegger/nvim-jdtls" },
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.hls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.terraformls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.kotlin_language_server.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-			})
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            "williamboman/mason.nvim",
+            opts = {
+                registries = {
+                    "github:nvim-java/mason-registry",
+                    "github:mason-org/mason-registry",
+                },
+            },
+        },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "jdtls",
+                    "tsserver",
+                    "pyright",
+                    "hls",
+                    "terraformls",
+                    "kotlin_language_server",
+                    "gopls",
+                },
+            })
+        end,
+    },
+    { "mfussenegger/nvim-jdtls" },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-			vim.keymap.set("n", "K", vim.lsp.buf.hover)
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
-			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help)
-			vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder)
-			vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder)
-			vim.keymap.set("n", "<space>wl", function()
-				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-			end)
-			vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition)
-			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename)
-			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action)
-			vim.keymap.set("n", "gr", vim.lsp.buf.references)
-			vim.keymap.set("n", "<space>f", function()
-				vim.lsp.buf.format({ async = true })
-			end)
-		end,
-	},
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.tsserver.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.pyright.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.hls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.terraformls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.kotlin_language_server.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.gopls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.htmx.setup({
+                capabilities = capabilities,
+            })
+
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+            vim.keymap.set("n", "K", vim.lsp.buf.hover)
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
+            vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help)
+            vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder)
+            vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder)
+            vim.keymap.set("n", "<space>wl", function()
+                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+            end)
+            vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition)
+            vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename)
+            vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action)
+            vim.keymap.set("n", "gr", vim.lsp.buf.references)
+            vim.keymap.set("n", "<space>f", function()
+                vim.lsp.buf.format({ async = true })
+            end)
+        end,
+    },
 }
--- lua/settings.lua
+++ lua/settings.lua
@@ -1,8 +1,8 @@
 vim.opt.mouse = ""
 vim.o.expandtab = true
-vim.o.tabstop = 2
-vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.clipboard = "unnamedplus"
