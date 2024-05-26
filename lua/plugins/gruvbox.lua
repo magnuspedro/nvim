3c80f0a84e6acbb03ccb8a53b96e95eb28f4b475
magnus <pedmagnus@gmail.com>
2024-05-26 19:33:44 -0300

    removing noice

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,6 +1,9 @@
 return {
-  "ellisonleao/gruvbox.nvim",
-  priority = 1000,
-  config = true,
-  opts = ...,
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("gruvbox")
+      vim.o.background = "dark"
+    end,
+    opts = ...,
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,56 +1,23 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = {
-    { "nvim-tree/nvim-web-devicons" },
-
-    {
-      "folke/noice.nvim",
-      event = "VeryLazy",
-      dependencies = {
-        "MunifTanjim/nui.nvim"
-      },
-      config = function()
-        require("noice").setup({
-          lsp = {
-            override = {
-              ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-              ["vim.lsp.util.stylize_markdown"] = true,
-              ["cmp.entry.get_documentation"] = true,
-            },
-          },
-          presets = {
-            bottom_search = true,
-            command_palette = true,
-            long_message_to_split = true,
-            inc_rename = false,
-            lsp_doc_border = false,
-          },
-        })
-      end,
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+        { "nvim-tree/nvim-web-devicons" },
     },
-  },
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "gruvbox_dark",
-        icons_enabled = true,
-        component_separators = "|",
-        section_separators = "",
-      },
-      sections = {
-        lualine_x = {
-          {
-            require("noice").api.statusline.mode.get,
-            cond = require("noice").api.statusline.mode.has,
-            color = { fg = "#ff9e64" },
-          },
-        },
-        lualine_a = {
-          {
-            "buffers",
-          },
-        },
-      },
-    })
-  end,
+    config = function()
+        require("lualine").setup({
+            options = {
+                theme = "gruvbox_dark",
+                icons_enabled = true,
+                component_separators = "|",
+                section_separators = "",
+            },
+            sections = {
+                lualine_a = {
+                    {
+                        "buffers",
+                    },
+                },
+            },
+        })
+    end,
 }
