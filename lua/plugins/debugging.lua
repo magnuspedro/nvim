a7290153dad946033abd7fe1dc61fd96c3d90ad8
magnus <pedmagnus@gmail.com>
2024-06-03 10:49:47 -0300

    adding go debugger

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,25 +1,57 @@
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = { "rcarriga/nvim-dap-ui", "nvim-neotest/nvim-nio"},
-  config = function()
-    local dap, dapui = require("dap"), require("dapui")
+    "mfussenegger/nvim-dap",
+    dependencies = { "rcarriga/nvim-dap-ui", "nvim-neotest/nvim-nio" },
+    config = function()
+        local dap, dapui = require("dap"), require("dapui")
 
-    dapui.setup({})
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function() end
-    dap.listeners.before.event_exited.dapui_config = function() end
+        dap.adapters.delve = {
+            type = "server",
+            port = "${port}",
+            executable = {
+                command = "dlv",
+                args = { "dap", "-l", "127.0.0.1:${port}" },
+            }
+        }
 
-    vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint, {})
-    vim.keymap.set("n", "<F5>", dap.continue, {})
-    vim.keymap.set("n", "<F7>", dap.step_into, {})
-    vim.keymap.set("n", "<F8>", dap.step_over, {})
-    vim.keymap.set("n", "<F9>", dap.step_out, {})
-    vim.keymap.set("n", "<F10>", dap.run_last, {})
-    vim.keymap.set("n", "<leader>bc", dapui.close, {})
-  end,
+        dap.configurations.go = {
+            {
+                type = "delve",
+                name = "Debug",
+                request = "launch",
+                program = "${file}",
+            },
+            {
+                type = "delve",
+                name = "Debug test",
+                request = "launch",
+                mode = "test",
+                program = "${file}",
+            },
+            {
+                type = "delve",
+                name = "Debug test (go.mod)",
+                request = "launch",
+                mode = "test",
+                program = "./${relativeFileDirname}",
+            }
+        }
+
+        dapui.setup({})
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function() end
+        dap.listeners.before.event_exited.dapui_config = function() end
+
+        vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint, {})
+        vim.keymap.set("n", "<F5>", dap.continue, {})
+        vim.keymap.set("n", "<F7>", dap.step_into, {})
+        vim.keymap.set("n", "<F8>", dap.step_over, {})
+        vim.keymap.set("n", "<F9>", dap.step_out, {})
+        vim.keymap.set("n", "<F10>", dap.run_last, {})
+        vim.keymap.set("n", "<leader>bc", dapui.close, {})
+    end,
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -72,6 +72,7 @@ return {
             vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename)
             vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action)
             vim.keymap.set("n", "gr", vim.lsp.buf.references)
+            vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
             vim.keymap.set("n", "<space>f", function()
                 vim.lsp.buf.format({ async = true })
             end)
