e74009422f74b667b077383da0a950626fbf50ba
magnus <pedmagnus@gmail.com>
2024-06-08 02:39:16 -0300

    adding go to none ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,13 +1,14 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local none_ls = require("null-ls")
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local none_ls = require("null-ls")
 
-		none_ls.setup({
-			sources = {
-				none_ls.builtins.formatting.stylua,
-				none_ls.builtins.formatting.google_java_format,
-			},
-		})
-	end,
+        none_ls.setup({
+            sources = {
+                none_ls.builtins.formatting.stylua,
+                none_ls.builtins.formatting.google_java_format,
+                none_ls.builtins.formatting.gofmt,
+            },
+        })
+    end,
 }
